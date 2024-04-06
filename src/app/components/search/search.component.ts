b2b9502928b6055f725b0dba78f083206385268a
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-04-06 04:10:03 +0100

    search inputs tracked

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { SearchbarComponent } from '../sharedComponents/searchbar/searchbar.component';
+import { TagsComponent } from './tags/tags.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [SearchbarComponent, TagsComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  itemsSelected = {Name:"", Genres: new Array<string>, Year:"", Season:"", Format: new Array<string>}
 
+  selectedHandler(option:{class:string, value:String}) {
+    if (option.class == 'Genres' || option.class == 'Format'){
+      this.itemsSelected[option.class].push(String(option.value));
+    }
+    if (option.class == 'Name' || option.class == 'Year' || option.class == 'Season'){
+      this.itemsSelected[option.class] = String(option.value);
+    }
+  }
 }
--- src/app/components/sharedComponents/searchbar/searchbar.component.ts
+++ src/app/components/sharedComponents/searchbar/searchbar.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, input } from '@angular/core';
 import { optionsBuilder } from "./optionsBuilder";
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-searchbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.css'
@@ -13,9 +14,14 @@ import { CommonModule } from '@angular/common';
 export class SearchbarComponent {
   optionsBuilder = new optionsBuilder();
   buttonsSelected = Array(4).fill(false);
-  currentPage = input();
+  name = new FormControl('');
+  @Output() itemsEmiiter = new EventEmitter();
 
   handleFilterClick = (option:number) => {
     this.buttonsSelected[option] = !this.buttonsSelected[option];
+  }
+
+  emitOptions(optionSelected?: {class:string, value:String}) {
+    this.itemsEmiiter.emit(optionSelected ? optionSelected : {class:'Name',value:this.name.value});
   }
 }
