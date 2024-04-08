b89edf52f474c382a60e0819f4314b516122ac92
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-04-08 22:38:03 +0100

    search-page done mock

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { SearchbarComponent } from '../sharedComponents/searchbar/searchbar.component';
 import { TagsComponent } from './tags/tags.component';
+import { PaginationComponent } from '../sharedComponents/pagination/pagination.component';
+import { ResultsComponent } from './results/results.component';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [SearchbarComponent, TagsComponent],
+  imports: [SearchbarComponent, TagsComponent, PaginationComponent, ResultsComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
