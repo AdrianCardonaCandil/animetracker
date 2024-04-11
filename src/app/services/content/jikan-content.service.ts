eb2d77a0173a6f1adbfd1f62bc2087bd4af4f111
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-04-12 00:38:28 +0100

    jikan nearly finished

--- src/app/services/content/jikan-content.service.ts
+++ src/app/services/content/jikan-content.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { __env } from '../../../environments/env.dev';
 
+type parameters = {Name:string, Genres:string[], Year:string, Season:string, Format:string, Page:string};
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,25 +11,6 @@ export class JikanContentService {
   private characterpath:string;
   private seasonpath:string;
 
-  private seasons = {
-    autumn:{
-        start:"10-01",
-        end:"12-31"
-    },
-    summer:{
-        start:"07-01",
-        end:"09-30"
-    },
-    spring:{
-        start:"04-01",
-        end:"06-30"
-    },
-    winter:{
-        start:"01-01",
-        end:"03-31"
-    }
-  }
-
   constructor() {
     this.contentpath = `${__env.JIKAN_PATH}/anime`;
     this.characterpath = `${__env.JIKAN_PATH}/characters`;
@@ -35,20 +18,30 @@ export class JikanContentService {
   }
   
   // Function to get a content by ID in Jikan
-  findById = (id:string) => fetch(`${this.contentpath}/${id}`).then(res => res.json()).then(res => res.data); // Works well
+  findById = (id:string) => fetch(`${this.contentpath}/${id}`).then(res => res.json()).then(res => res.data); // Works
 
   // Function to get the characters of a content finding by ID.
-  findCharacters = (id:string) => fetch(`${this.contentpath}/${id}/characters`).then(res => res.json()).then(res => res.data); // Works well
+  findCharacters = (id:string) => fetch(`${this.contentpath}/${id}/characters`).then(res => res.json()).then(res => res.data); // Works
 
   // Function to get the episodes of a content finding by ID.
-  findEpisodes = (id:string, page?:string) => fetch(`${this.contentpath}/${id}/episodes?page=${page}`).then(res => res.json()).then(res => page ? (page <= res.pagination.last_visible_page ? res.data : []) : res.data); // Works well
+  findEpisodes = (id:string, page:string) => fetch(`${this.contentpath}/${id}/episodes?page=${page}`).then(res => res.json()); // Works
 
   // Function to get the images of a content finding by ID.
-  findImages = (id:string) => fetch(`${this.contentpath}/${id}/pictures`).then(res => res.json()).then(res => res.data);
+  findImages = (id:string) => fetch(`${this.contentpath}/${id}/pictures`).then(res => res.json()).then(res => res.data); // Works
 
   // Function to get a characters description finding by character ID.
-  findCharacterDescription = (characterid:string) => fetch(`${this.characterpath}/${characterid}`).then(res => res.json()).then(res => res.data.about ? res.data.about : ''); // Works well
+  findCharacterDescription = (characterid:string) => fetch(`${this.characterpath}/${characterid}`).then(res => res.json()).then(res => res.data.about ? res.data.about : ''); // Works
 
   // Function to get a full content season finding by year and season.
-  findSeasonContents = (year:string, season:string, format?:string, page?:string) => fetch(`${this.seasonpath}/${year}/${season}?${format ? `filter=${format}&` : ''}${page ? `page=${page}`: ''}`).then(res => res.json()).then(res => page ? (page <= res.pagination.last_visible_page ? res.data : []) : res.data); // Works well
+  private findSeasonContents = (year:string, season:string, format:string, page:string) => fetch(`${this.seasonpath}/${year}/${season}?${format !== '' ? `filter=${format}&` : ''}${page !== '' ? `page=${page}`: ''}`).then(res => res.json());
+
+  private findNameGenres = (name:string, genres:string[], format:string, page:string) => fetch(`${this.contentpath}?${name !== "" ? `q=${name}&` : ''}${genres.length !== 0 ? `genres=${genres.join(',')}&` : ''}${format !== '' ? `type=${format}&` : ''}${page !== '' ? `page=${page}` : ''}`).then(res => res.json());
+
+  find({Name, Genres, Year, Season, Format, Page}:parameters){
+    if(Year || Season){
+      return this.findSeasonContents(Year, Season, Format, Page);
+    } else {
+      return this.findNameGenres(Name, Genres, Format, Page);
+    }
+  }
 }
