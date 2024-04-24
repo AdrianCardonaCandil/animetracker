2d587fb5b6eff67d2f4ff4fd0802ed4eeb1cb659
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-04-24 19:47:28 +0100

    user service

---
+++ src/app/services/user/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import Users from "../../models/user.model";
+import { FirebaseUserService } from "./firebase-user.service";
+import User from "../../schemas/User.scheme";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService implements Users {
+
+  constructor(private firebaseAuthService: FirebaseUserService) {}
+
+  findById = async (id: string): Promise<User | null> => this.firebaseAuthService.findById(id);
+  find = async (queryObj: Object): Promise<User[] | null> => this.firebaseAuthService.find(queryObj);
+  findOne = async (queryObj: Object): Promise<User | null> => this.firebaseAuthService.findOne(queryObj);
+
+  getContentsFromList = async (userId: string, listField: string): Promise<{ [key: string]: any } | null> =>
+    this.firebaseAuthService.getContentsFromList(userId, listField) as Promise<{ [key: string]: any } | null>;
+
+  checkOnList = async (userId: string, contentId: string, nameList: string): Promise<boolean> =>
+    this.firebaseAuthService.checkOnList(userId, contentId, nameList);
+
+  trackingList = async (userId: string, contentId: string, listField: string): Promise<boolean> =>
+    this.firebaseAuthService.trackingList(userId, contentId, listField);
+
+  isOnList = async (userId: string, contentId: string): Promise<string | null> =>
+    this.firebaseAuthService.isOnList(userId, contentId);
+
+  incrementEpisodesCount = async (userId: string, contentId: string): Promise<number> =>
+    this.firebaseAuthService.incrementEpisodesCount(userId, contentId);
+
+  decrementEpisodesCount = async (userId: string, contentId: string): Promise<number> =>
+    this.firebaseAuthService.decrementEpisodesCount(userId, contentId);
+}
