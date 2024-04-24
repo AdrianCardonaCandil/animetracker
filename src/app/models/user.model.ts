00ae328d065d45a17e8997b744c09692cc0936e7
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-04-24 18:46:30 +0100

    added user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,2 +1,14 @@
-export class User {
+import User from "../schemas/User.scheme";
+
+export default interface Users {
+  findById: (id: string) => Promise<User | null>
+  findByUsername: (username: string) => Promise<User | null>
+  find: (obj: Object) => Promise<Array<User>>
+  findOne: (obj: Object) => Promise<User | null>
+  getContentsFromList: (userId: string, listField: string) => Promise<Array<Map<string, any> | null>>
+  checkOnList: (userId: string | undefined, contentId: string | undefined, listField: string) => Promise<boolean>
+  trackingList: (userId: string | undefined, contentId: string | undefined, listField: string) => Promise<void>
+  isOnList: (userId: string | undefined, contentId: string | undefined) => Promise<string | null>
+  incrementEpisodesCount: (userId: string | undefined, contentId: string | undefined) => Promise<number>
+  decrementEpisodesCount: (userId: string | undefined, contentId: string | undefined) => Promise<number>
 }
