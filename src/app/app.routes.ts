5bfe0a9d8a8132d070781a3298c4968f2f85bb06
ElenaMoralesGil <elena.morales104@alu.ulpgc.es>
2024-04-25 17:33:46 +0100

    routing for profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ContentComponent } from './components/content/content.component';
 import { CharactersComponent } from './components/content/characters/characters.component';
 import { EpisodesComponent } from './components/content/episodes/episodes.component';
 import { SearchComponent } from './components/search/search.component';
+import {TableComponent} from "./components/profile/table/table.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
 export const routes: Routes = [
     {
@@ -25,5 +27,15 @@ export const routes: Routes = [
     },
     {
         'path':'search/:page', component:SearchComponent
-    }
+    }, { path: "profile/:username", redirectTo: "profile/:username/watching", pathMatch: "full" },
+  {
+    path: 'profile/:username', component: ProfileComponent,
+    children: [
+      { path: 'watching', component: TableComponent },
+      { path: 'completed', component: TableComponent },
+      { path: 'planToWatch', component: TableComponent },
+      { path: 'favorites', component: TableComponent },
+      { path: 'dropped', component: TableComponent }
+    ]
+  }
 ];
