6ef39e1c6c982334a69582af60d93e31afece009
AdrianCardonaCandil <driu.cardona@gmail.com>
2024-05-03 17:44:12 +0100

    header included in git

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,63 @@
+import { booleanAttribute, Component, EventEmitter, input, Output } from '@angular/core';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { RouterLink } from "@angular/router";
+import { CommonModule, NgIf } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms"; // Import SignInComponent
+import { Observable } from 'rxjs';
+import User from '../../schemas/User.scheme'
+import { AuthService } from '../../services/auth/auth.service';
+import {MobileMenuComponent} from "./mobile-menu/mobile-menu.component";
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  templateUrl: './header.component.html',
+  imports: [
+    CommonModule,
+    SignInComponent,
+    SignUpComponent,
+    RouterLink,
+    NgIf,
+    FormsModule,
+    ReactiveFormsModule,
+    SignInComponent,
+    MobileMenuComponent
+  ],
+  styleUrls: [ './header.component.css' ]
+})
+export class HeaderComponent {
+  protected userLogged$!: Observable<User | null>
+  showSignInModal = false;
+  showSignUpModal = false;
+  showMobileMenu = false;
+  constructor(private Auth: AuthService) {
+    this.userLogged$ = Auth.user
+  }
+
+  showSignIn() {
+    this.showSignInModal = true;
+    this.showSignUpModal = false;
+  }
+
+  showSignUp() {
+    this.showSignInModal = false;
+    this.showSignUpModal = true;
+  }
+
+  closeSignInModal() {
+    this.showSignInModal = false;
+  }
+
+  closeSignUpModal() {
+    this.showSignUpModal = true;
+  }
+
+  logout() {
+    this.Auth.logout()
+  }
+
+  mobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu
+  }
+}
---
+++ src/app/components/header/sign-up/sign-up.component.ts
@@ -0,0 +1,77 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, FormsModule } from '@angular/forms'; // Import AbstractControl
+import { NgClass, NgIf } from "@angular/common";
+import { AuthService } from "../../../services/auth/auth.service";
+import { ActivatedRoute } from "@angular/router"; // Import Users as a named export
+
+@Component({
+  selector: 'app-sign-up',
+  standalone: true,
+  templateUrl: './sign-up.component.html',
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgIf,
+    NgClass
+  ],
+  styleUrl: './sign-up.component.css'
+})
+export class SignUpComponent {
+  @Output() signUp = new EventEmitter();
+  form: FormGroup;
+  errorSignUp: boolean = false;
+  description = "";
+  profilePicture = "";
+  constructor(private formBuilder: FormBuilder, private Auth: AuthService) {
+    this.form = this.formBuilder.group({
+      username: [ '', [ Validators.required, Validators.minLength(2) ] ],
+      email: [ '', [ Validators.required, Validators.email ] ],
+      password: [ '', [ Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/u
+      ) ] ],
+      repeat_password: [ '', Validators.required ]
+    }, {
+      validator: this.passwordMatchValidator
+    });
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      const { username, email, password } = this.form.value
+      try {
+      this.Auth.signUp({ username, email, password })
+        .then((user)=> {
+          if (user !== null) {
+            this.Auth.signIn({ username, password })
+            this.closesignUp()
+            alert('SignUp successful');
+          } else {
+            this.errorSignUp = true;
+          }
+          })
+        } catch (error) {
+          console.error('Error during login:', error);
+          this.errorSignUp = true;
+        }
+    }
+    else console.log("NO SUBMIT")
+  }
+
+  closesignUp() {
+    this.signUp.emit();
+  }
+
+  // Custom validator function
+  passwordMatchValidator(control: AbstractControl): { [ key: string ]: boolean } | null {
+    const password = control.get('password');
+    const repeatPassword = control.get('repeat_password');
+
+    if (password && repeatPassword && password.value !== repeatPassword.value) {
+      return { 'passwordMismatch': true };
+    }
+
+    return null;
+  }
+}
+
+
+
